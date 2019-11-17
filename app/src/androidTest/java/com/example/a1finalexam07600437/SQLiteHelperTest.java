9266822c4d35ff6ffa88e4716340b5ba2dd3a2c7
ketsirin <ketsirin007@gmail.com>
2019-11-17 12:21:13 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/example/a1finalexam07600437/SQLiteHelperTest.java
@@ -0,0 +1,7 @@
+package com.example.a1finalexam07600437;
+
+import static org.junit.Assert.*;
+
+public class SQLiteHelperTest {
+
+}
---
+++ app/src/main/java/com/example/a1finalexam07600437/SQLiteHelper.java
@@ -0,0 +1,24 @@
+package com.example.a1finalexam07600437;
+
+import android.database.sqlite.SQLiteDatabase;
+
+class SQLiteHelper {
+    public static final String TABLE_NAME = ;
+    public static final String DATABASE_NAME = ;
+    public static String Table_Column_2_Email;
+    public static String Table_Column_ID;
+    public static Object Table_Column_3_Password;
+    public static Object Table_Column_1_Name;
+
+    public SQLiteHelper(MainActivity mainActivity) {
+    }
+
+    public SQLiteDatabase getWritableDatabase() {
+    }
+
+    public class Table_Column_3_Password {
+    }
+
+    public class Table_Column_1_Name {
+    }
+}
